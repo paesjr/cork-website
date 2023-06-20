5add68c262b3f1ba48222c381633f77acaf0015c
Ricardo Paes <ricardo.paes@learnerme.academy>
2023-06-21 09:31:57 +1200

    Save local changes to Config.js

--- src/components/chatbot/Config.js
+++ src/components/chatbot/Config.js
@@ -1,8 +1,6 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 import BotAvatar from "./BotAvatar";
 
-// Config starter code
-
 const config = {
   initialMessages: [createChatBotMessage(`Welcome to CORK bodyboards!`)],
   botName: "CORK BOT",
